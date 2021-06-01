576d16fe21b512093574745cdce9e89728799297
Aksh Sood <sood_aksh_2001@outlook.com>
2021-06-01 16:56:24 +0530

    changes to utilities and registerationscreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:to_doc_patient/screens/loginScreen.dart';
 import 'package:to_doc_patient/screens/splashScreen.dart';
 import 'package:to_doc_patient/utilities/pallete.dart';
-import 'package:to_doc_patient/screens/registerScreen.dart';
+import 'package:to_doc_patient/screens/registerScreen1.dart';
 import 'package:to_doc_patient/screens/forgotPassword.dart';
 import 'package:to_doc_patient/screens/homeScreen.dart';
 void main() {
---
+++ lib/utilities/inputTile.dart
@@ -0,0 +1,39 @@
+import "package:flutter/material.dart";
+import 'package:to_doc_patient/utilities/constants.dart';
+import 'package:to_doc_patient/utilities/pallete.dart';
+import 'package:flutter/services.dart';
+
+class InputTile extends StatelessWidget {
+  InputTile({
+    @required this.isObscure,
+    this.tileIcon,
+    @required this.inputType,
+    @required this.variable,
+    this.textLength,
+  });
+  String variable;
+  final String inputType;
+  final bool isObscure;
+  final IconButton tileIcon;
+  final List<TextInputFormatter> textLength;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      inputFormatters: textLength,
+      style: kTextFieldStyle,
+      onChanged: (value) {
+        variable = value;
+      },
+      obscureText: isObscure,
+      decoration: InputDecoration(
+          filled: true,
+          fillColor: Palette.kSecondaryColor,
+          hintText: inputType,
+          hintStyle: kHintTextStyle,
+          suffixIcon: tileIcon,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              borderSide: BorderSide.none)),
+    );
+  }
+}
